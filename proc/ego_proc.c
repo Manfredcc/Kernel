84b9afa40fb77a43fda4b7a47539dcc96578402d
Manfred <1259106665@qq.com>
2023-07-23 13:14:39 +0800

    Add kernel/proc method to debug value

---
+++ proc/ego_proc.c
@@ -0,0 +1,111 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+static bool debug_option = true;    /* hard-code control */
+
+#define ego_err(chip, fmt, ...)     \
+    pr_err("%s: %s " fmt, chip->name,   \
+        __func__, ##__VA__ARGS__)
+
+#define ego_info(chip, fmt, ...)    \
+    do {                            \
+        if (chip->debug_on && debug_option)        \
+            pr_info("%s: %s " fmt, chip->name, \
+                __func__, ##__VA_ARGS__);       \
+        else                                    \
+            ;   \
+    } while(0)
+
+typedef struct _egoist {
+    char *name;
+    bool debug_on;
+    int proc_val;
+}egoist, *pegoist;
+pegoist chip;
+
+static int ego_proc_show(struct seq_file *m, void *v)
+{
+    seq_printf(m, "proc_val:%d\n", chip->proc_val);
+
+    return 0;
+}
+
+static int ego_seq_open(struct inode *inode, struct file *filp)
+{
+    return single_open(filp, ego_proc_show, NULL);
+}
+
+#define MAX_BUFF    20
+ssize_t ego_proc_write(struct file *filp, const char *buf, size_t size, loff_t *pos)
+{
+    int ret;
+    char val[MAX_BUFF];
+
+    if (copy_from_user(&val, buf, size))
+        return -EFAULT;
+
+    val[size] = '\0';
+
+    ret = kstrtoint(val, 0, &chip->proc_val);
+    
+    return size;
+}
+
+static const struct proc_ops ego_proc_ops = {
+    .proc_open = ego_seq_open,
+    .proc_read = seq_read,
+    .proc_write = ego_proc_write,
+};
+
+void ego_release(pegoist chip)
+{
+    if (chip != NULL) {
+        /* Nothins to do in this module */
+    } else {
+        pr_err("Failed to alloc mem for egoist\n");
+    }
+}
+
+static int __init ego_print_init(void)
+{
+    int ret = 0;
+
+    do {
+        chip = kzalloc(sizeof(*chip), GFP_KERNEL);
+        if (!chip) {
+            ret = ENOMEM;
+            break;
+        }
+
+        chip->name = "egoist";
+        chip->debug_on = true;
+        proc_create("ego_proc", 0660, NULL, &ego_proc_ops);
+
+    } while (0);
+
+    if (ret) {
+        ego_release(chip);
+        return ret;
+    }
+    
+    ego_info(chip, "All things goes well, awesome\n");
+    return ret;
+}
+
+static void __exit ego_print_exit(void)
+{
+    remove_proc_entry("ego_proc", NULL);
+    ego_release(chip);
+    pr_info("All things gone\n");
+}
+
+module_init(ego_print_init);
+module_exit(ego_print_exit);
+
+MODULE_AUTHOR("Manfred <1259106665@qq.com>");
+MODULE_LICENSE("GPL");
